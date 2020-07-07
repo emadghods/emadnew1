4c5400a7dbeee33ff869ebb95d64b194a166f2b4
emadghods <emad.ghods@gmail.com>
2020-07-07 19:39:50 +0430

    class new add shad

--- src/emadnewe1.java
+++ src/emadnewe1.java
@@ -1,13 +1,5 @@
-import java.util.*
+import java.util.*;
 public class emadnewe1 {
-    public static void main (String args []){
-        Scanner vorodi = new Scanner (System.in);
-        Syste.out.println (" please enter your name");
-
-
-
+    int a;
 
     }
-
-
-}
