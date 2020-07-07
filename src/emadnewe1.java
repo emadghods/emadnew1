e04cb86abbc629b63b004cfd778e4f5e20e356fa
emadghods <emad.ghods@gmail.com>
2020-07-07 19:30:21 +0430

    Initial commit

---
+++ src/emadnewe1.java
@@ -0,0 +1,13 @@
+import java.util.*
+public class emadnewe1 {
+    public static void main (String args []){
+        Scanner vorodi = new Scanner (System.in);
+        Syste.out.println (" please enter your name");
+
+
+
+
+    }
+
+
+}
